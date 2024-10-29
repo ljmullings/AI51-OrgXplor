775a5ea85fc74c9c3d6bcc2e702467205045824a
unclethien <dxn210021@utdallas.edu>
2024-10-29 10:41:03 -0500

    update

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+    "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+    "./app/(tabs)/meditate.tsx",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        rmono: ["Roboto-Mono", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
