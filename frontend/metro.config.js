880518a218d86f1753576de97e265d7d7ef430ee
unclethien <dxn210021@utdallas.edu>
2024-11-25 01:32:27 -0600

    update

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,5 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
--- frontend/utils/firebase.ts
+++ frontend/utils/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
@@ -9,18 +10,17 @@ import { initializeApp } from 'firebase/app';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDiYkNm5i1DR6dnEra8SZsKUI86Nf3f64k',
+  authDomain: 'testing-9fa87.firebaseapp.com',
+  projectId: 'testing-9fa87',
+  storageBucket: 'testing-9fa87.firebasestorage.app',
+  messagingSenderId: '7396458111',
+  appId: '1:7396458111:web:5ab5a4c8a456aaee1b094a',
 };
 
 const firebase = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
-
+const firebaseAuth = getAuth(firebase);
 export default firebase;
+export { firebaseAuth };
